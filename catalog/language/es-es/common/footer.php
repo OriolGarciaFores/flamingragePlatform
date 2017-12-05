c195203a4b8c9b560dc9d866aeb93fd2a5e91769
OriolGarciaFores <ori_ugf@hotmail.com>
2017-12-05 21:43:47 +0100

    translate footer

--- catalog/language/es-es/common/footer.php
+++ catalog/language/es-es/common/footer.php
@@ -3,7 +3,7 @@
 $_['text_information']  = 'Information';
 $_['text_service']      = 'Customer Service';
 $_['text_extra']        = 'Extras';
-$_['text_contact']      = 'Contact Us';
+$_['text_contact']      = 'CONTACTAR';
 $_['text_return']       = 'Returns';
 $_['text_sitemap']      = 'Site Map';
 $_['text_manufacturer'] = 'Brands';
@@ -15,3 +15,10 @@ $_['text_order']        = 'Order History';
 $_['text_wishlist']     = 'Wish List';
 $_['text_newsletter']   = 'Newsletter';
 $_['text_powered']      = 'Powered By <a href="http://www.opencart.com">OpenCart</a><br /> %s &copy; %s';
+
+$_['text_about_us']     = 'QUIENES SOMOS';
+$_['text_interesting_sites']     = 'SITIOS DE INTERÉS';
+$_['text_about_web']    = 'Sobre la web';
+$_['text_team']         = "Equipo";
+$_['text_rules']        = "Normativa";
+$_['text_support']        = "Centro de soporte";
