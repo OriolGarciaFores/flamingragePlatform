a219a366b2825cb065dc842dbb389d47f72b47c8
OriolGarciaFores <ori_ugf@hotmail.com>
2017-12-06 23:56:07 +0100

    add not found

--- catalog/language/es-es/error/not_found.php
+++ catalog/language/es-es/error/not_found.php
@@ -1,6 +1,6 @@
 <?php
 // Heading
-$_['heading_title'] = 'The page you requested cannot be found!';
+$_['heading_title'] = '404';
 
 // Text
-$_['text_error']    = 'The page you requested cannot be found.';
+$_['text_error']    = 'LA PÁGINA SOLICITADA NO SE ENCUENTRA.<br /> POR FAVOR VUELVA A LA PÁGINA PRINCIPAL, O UTILICE EL MENÚ';
